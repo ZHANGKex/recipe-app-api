bd4705ce3c320a1fc213701392a26df3bb03c6e8
ZHANGKex <101405395+ZHANGKex@users.noreply.github.com>
2024-09-01 15:32:06 +0200

    add TDD exercide, and add psycopg2 to requirements and dockerfile

---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
