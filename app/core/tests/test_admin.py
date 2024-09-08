93417ffc285a85a5f5de7870a9d16fcc1fcc9692
ZHANGKex <101405395+ZHANGKex@users.noreply.github.com>
2024-09-08 16:42:59 +0200

    edit django admin page and tests

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -9,10 +9,10 @@ from django.test import Client
 
 class AdminSiteTests(TestCase):
     """Test for Django admin"""
-    def setup(self):
+    def setUp(self):
         """Create user and client"""
         self.client = Client()
-        self.admin_user = get_user_model.objects.create_superuser(
+        self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
             password='testpass123',
         )
@@ -23,10 +23,22 @@ class AdminSiteTests(TestCase):
             name='Test User',
         )
 
-
     def test_users_list(self):
         """Test that users are listed on page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_create_user_page(self):
+        """Test the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
