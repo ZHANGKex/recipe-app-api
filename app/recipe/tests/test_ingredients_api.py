11dc0ccfb3cbb8d3cfecc9e8746c860fd516d116
ZHANGKex <101405395+ZHANGKex@users.noreply.github.com>
2024-09-18 21:54:29 +0200

    implement update recipe with ingredients feature and add tests

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -92,5 +92,5 @@ class PrivateIngredientsApiTests(TestCase):
         res = self.client.delete(url)
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
-        ingredients = ingredient.objects.filter(user=self.user)
+        ingredients = Ingredient.objects.filter(user=self.user)
         self.assertFalse(ingredients.exists())
